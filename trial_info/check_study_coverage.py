a4614b0e798ed739e718bc16a98228cf5e4a4324
nnaik39 <nanni.naik@gmail.com>
2024-01-19 08:33:59 -0800

    updated study coverage

--- trial_info/check_study_coverage.py
+++ trial_info/check_study_coverage.py
@@ -4,7 +4,8 @@ with the datapoints that have yet to be covered.
 '''
 
 import json
-from math import e 
+from math import e
+from tracemalloc import start 
 
 f = open('ig-vqa-default-rtdb-description-editing-final-export.json')
 study_info = json.load(f)
@@ -19,31 +20,39 @@ pilot_exp = json.load(f)
 answers = {}
 collected_datapoints = []
 
+start_description_per_image = {}
+
 for participant in study_info:
     for trial in study_info[participant]:
         found_in_pilot_exp = False 
+
         for pilot_exp_entry in pilot_exp['images']:
             if (trial['picture'] == pilot_exp_entry['filename']):
                     found_in_pilot_exp = True
         
         if (found_in_pilot_exp):
-            if (((trial['picture'], trial['category'], trial['start_description'])) not in answers):
-                answers[(trial['picture'], trial['category'], trial['start_description'])] = []
-            answers[(trial['picture'], trial['category'], trial['start_description'])].append((trial['picture'], trial['category'], trial['start_description']))
+            if (((trial['picture'])) not in answers):
+                answers[(trial['picture'])] = []
+            answers[(trial['picture'])].append((trial['final_description']))
 
-for (image, context, description) in answers:
-    if (len(answers[(image, context, description)]) >= 3):
+            if (trial['picture'] not in start_description_per_image):
+                start_description_per_image[trial['picture']] = trial['start_description']
+
+print("Number of images: ", len(answers))
+
+for image in answers:
+    if (len(answers[(image)]) >= 3):
         i = {
             'image': image,
-            'context': context,
-            'answers': answers[(image, context, description)]}
+            'answers': answers[(image)]}
         collected_datapoints.append(i)
     else:
         new_pilot_exp['images'].append({
             'filename': image,
-            'category': context,
-            'description': description
+            'description': start_description_per_image[image]
         })
+
+print("Number of datapoints left: ", len(new_pilot_exp))
 
 with open("new_pilot_exp.json", "w") as outfile:
     outfile.write(json.dumps(new_pilot_exp, indent = 4))
