6fb0ffa93cb3a332f4572d15a41e76f47eab95bc
nnaik39 <nanni.naik@gmail.com>
2024-05-02 13:35:48 -0700

    updated study coverage to remove 100 description edits

--- trial_info/check_study_coverage.py
+++ trial_info/check_study_coverage.py
@@ -6,7 +6,7 @@ with the datapoints that have yet to be covered.
 import json
 from math import e
 
-f = open('ig-vqa-default-rtdb-description-editing-final-export.json')
+f = open('/Users/nanditanaik/Downloads/ig-vqa-default-rtdb-description-editing-1000-images-expansion-export (1).json')
 study_info = json.load(f)
 
 f = open('old_context_assignments.json')
@@ -24,23 +24,21 @@ collected_datapoints = []
 
 start_description_per_image = {}
 
-print("Number of images in old context assignments: ", len(pilot_exp['images']))
+for i in context_assignments:
+    found_in_pilot_exp = False
 
-#for i in context_assignments['images']:
-#    found_in_pilot_exp = False
+    for participant in study_info:
+        for trial in study_info[participant]:
+            if (trial['picture'] == i['filename']):
+                found_in_pilot_exp = True
 
-#    for participant in study_info:
- #       for trial in study_info[participant]:
-  #          if (trial['picture'] == i['filename']):
-   #             found_in_pilot_exp = True
-
-#    if (not found_in_pilot_exp):
- #       if ({'filename': i['filename'],
-  #          'description': i['description']} not in new_pilot_exp['images']):
- #           new_pilot_exp['images'].append({
-  #                  'filename': i['filename'],
-   #                 'description': i['description']
-   #         })
+    if (not found_in_pilot_exp):
+        if ({'filename': i['filename'],
+            'description': i['description']} not in new_pilot_exp['images']):
+                new_pilot_exp['images'].append({
+                    'filename': i['filename'],
+                    'description': i['description']
+            })
             
 #with open("new_pilot_exp.json", "w") as outfile:
  #   outfile.write(json.dumps(new_pilot_exp, indent = 4))
@@ -48,6 +46,11 @@ print("Number of images in old context assignments: ", len(pilot_exp['images']))
 for participant in study_info:
     for trial in study_info[participant]:
         found_in_pilot_exp = False 
+
+        if (trial['comments'] != ''):
+            print("Comments: ", trial['comments'])
+        if (trial['glb_comments'] != ''):
+            print("General comments: ", trial['glb_comments'])
 
         for pilot_exp_entry in pilot_exp['images']:
             if (trial['picture'] == pilot_exp_entry['filename']):
@@ -61,12 +64,13 @@ for participant in study_info:
             if (trial['picture'] not in start_description_per_image):
                 start_description_per_image[trial['picture']] = trial['start_description']
 
-print("Number of images: ", len(answers))
-
-print("Collected answers: ", answers)
-
+answers_so_far = []
 for image in answers:
-    if (len(answers[(image)]) >= 3):
+    i = {'image': image, 'answers': answers[(image)]}
+
+    answers_so_far.append(i)
+
+    if (len(answers[(image)]) >= 2):
         i = {
             'image': image,
             'answers': answers[(image)]}
@@ -77,12 +81,15 @@ for image in answers:
             'description': start_description_per_image[image]
         })
 
-print("Number of datapoints left: ", len(new_pilot_exp['images']))
+print("Number of datapoints left for full coverage: ", len(new_pilot_exp['images']))
+print("Total number of descriptions seen: ", len(answers))
+print("number of images seen: ", len(answers_so_far))
 
 with open("new_pilot_exp.json", "w") as outfile:
     outfile.write(json.dumps(new_pilot_exp, indent = 4))
 
-print("Number of collected description edits: ", len(collected_datapoints))
+with open("collected_description_datapoints.json", "w") as outfile:
+    outfile.write(json.dumps(collected_datapoints, indent = 4))
 
-#with open("collected_description_datapoints.json", "w") as outfile:
-#    outfile.write(json.dumps(collected_datapoints, indent = 4))
+with open('answers_so_far.json', 'w') as outfile:
+    outfile.write(json.dumps(answers_so_far, indent = 4))
